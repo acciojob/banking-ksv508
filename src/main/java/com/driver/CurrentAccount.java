749b3e31adfebdfbb1e7738b035990720fef579a
keshav <ksv508@gmail.com>
2023-06-19 10:39:13 +0530

    3rd

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -29,15 +29,17 @@ public class CurrentAccount extends BankAccount{
         for(int i = 0 ; i < n-1 ; i++){
             if(tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i+1))
                 break;
-            else
-                if(tradeLicenseId.charAt(i) != tradeLicenseId.charAt(i+1))
+            else {
+                if (tradeLicenseId.charAt(i) != tradeLicenseId.charAt(i + 1))
                     temp++;
+            }
         }
         if(temp == n-1)
             return;
         int freq[] = new int[26];
         for(int i = 0 ;i < n; i++)
             freq[tradeLicenseId.charAt(i) -'A']++;
+
         int max = 0, letter = 0;
         for(int i = 0; i < 26 ; i++){
             if(max < freq[i]){
@@ -45,14 +47,21 @@ public class CurrentAccount extends BankAccount{
                 letter = i;
             }
         }
+
         if(max > (n+1)/2)
             throw new Exception("Valid License can not be generated");
+
         char ch[] = new char[n];
-        for(int i = 0 ;i < n; i+=2) {
-            if(freq[letter] > n){
+
+        for(int i = 1 ;i < n; i+=2)
+        {
+            if(freq[letter] > 0)
+            {
                 ch[i] = (char) (letter + 'A');
                 freq[letter]--;
-            } else {
+            }
+            else
+            {
                 for (int j = 0; j < 26; j++) {
                     if (freq[j] > 0) {
                         letter = j;
